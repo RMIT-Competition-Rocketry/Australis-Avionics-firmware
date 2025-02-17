0296102573529ae937c20c7a5fbb919dcae3193f
Matt Ricci <s3785111@student.rmit.edu.au>
2025-02-17 17:53:46 +1100

    Fix outdated references in shell sources

--- Australis-Avionics/Core/Src/shell/baro.c
+++ Australis-Avionics/Core/Src/shell/baro.c
@@ -5,40 +5,39 @@
  *                                                                                 *
  * @{                                                                              *
  ***********************************************************************************/
- 
+
 #include "baro.h"
 
 static void Baro_exec(Shell *shell, uint8_t *);
 
 static ShellProgramHandle_t registerShellProgram() {
-	return (ShellProgramHandle_t){
-		.name = "baro",
-		.exec = Baro_exec
-	};
+  return (ShellProgramHandle_t){
+      .name = "baro",
+      .exec = Baro_exec
+  };
 }
 
-__attribute__((section(".shell_baro"), unused))
-static ShellProgramHandle_t (*registerShellProgram_ptr)() = registerShellProgram;
+__attribute__((section(".shell_baro"), unused)) static ShellProgramHandle_t (*registerShellProgram_ptr)() = registerShellProgram;
 
 /* =============================================================================== */
 /**
- * @brief 
+ * @brief
  *
  *
  **
  * =============================================================================== */
 static void Baro_exec(Shell *shell, uint8_t *flags) {
-	BMP581 *baro = DeviceHandle_getHandle("Baro").device;
-	char str[30];
-	if (!strcmp(flags, "read")) {
-		baro->update(baro);
-		snprintf(str, 50, "Ground pressure: %f\n\r", baro->groundPress);
-		shell->usb.print(&shell->usb, str);
-		snprintf(str, 50, "Current pressure: %f\n\r", baro->press);
-	  shell->usb.print(&shell->usb, str);
-		snprintf(str, 50, "Current temperature: %f\n\r", baro->temp);
-		shell->usb.print(&shell->usb, str);
-	}
+  BMP581 *baro = DeviceList_getDeviceHandle(DEVICE_BARO).device;
+  char str[30];
+  if (!strcmp(flags, "read")) {
+    baro->update(baro);
+    snprintf(str, 50, "Ground pressure: %f\n\r", baro->groundPress);
+    shell->usb.print(&shell->usb, str);
+    snprintf(str, 50, "Current pressure: %f\n\r", baro->press);
+    shell->usb.print(&shell->usb, str);
+    snprintf(str, 50, "Current temperature: %f\n\r", baro->temp);
+    shell->usb.print(&shell->usb, str);
+  }
 }
 
 /** @} */
--- Australis-Avionics/Core/Src/shell/launch.c
+++ Australis-Avionics/Core/Src/shell/launch.c
@@ -5,34 +5,34 @@
  *                                                                                 *
  * @{                                                                              *
  ***********************************************************************************/
- 
+
+#include "devicelist.h"
 #include "launch.h"
 
 static void Launch_exec(Shell *, uint8_t *);
-	
+
 static ShellProgramHandle_t registerShellProgram() {
-	return (ShellProgramHandle_t){
-		.name = "launch",
-		.exec = Launch_exec
-	};
+  return (ShellProgramHandle_t){
+      .name = "launch",
+      .exec = Launch_exec
+  };
 }
 
-__attribute__((section(".shell_launch"), unused))
-static ShellProgramHandle_t (*registerShellProgram_ptr)() = registerShellProgram;
+__attribute__((section(".shell_launch"), unused)) static ShellProgramHandle_t (*registerShellProgram_ptr)() = registerShellProgram;
 
 /* =============================================================================== */
 /**
- * @brief 
+ * @brief
  *
  *
  **
  * =============================================================================== */
 static void Launch_exec(Shell *shell, uint8_t *flags) {
-	DeviceHandle_t accelHandle = DeviceHandle_getHandle("Accel");
-	KX134_1211 *accel          = accelHandle.device;
-	accel->accelData[ZINDEX] = ACCEL_LAUNCH;
-	TaskHandle_t handle = xTaskGetHandle("StateUpdate");
-	xTaskAbortDelay(handle);
+  DeviceHandle_t accelHandle = DeviceList_getDeviceHandle(DEVICE_ACCEL);
+  KX134_1211 *accel          = accelHandle.device;
+  accel->accelData[ZINDEX]   = ACCEL_LAUNCH;
+  TaskHandle_t handle        = xTaskGetHandle("StateUpdate");
+  xTaskAbortDelay(handle);
 }
 
 /** @} */
