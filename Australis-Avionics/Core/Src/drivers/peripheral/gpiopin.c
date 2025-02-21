43ba397ff2a37228c9fa13d353c7eb29bfde8c3f
Matt Ricci <s3785111@student.rmit.edu.au>
2025-02-22 00:47:26 +1100

    Update GPIO pin peripheral driver with private initialiser for GPIO configuration registers, implement GPIOpin_updateConfig, various updates to internal documentation for driver.

--- Australis-Avionics/Core/Src/drivers/peripheral/gpiopin.c
+++ Australis-Avionics/Core/Src/drivers/peripheral/gpiopin.c
@@ -11,6 +11,8 @@
 #include "stddef.h"
 #include "stdlib.h"
 
+static void _GPIOpin_init(GPIO_TypeDef *, GPIO_Pin, GPIO_Config);
+
 /* =============================================================================== */
 /**
  * @brief Initialiser for a GPIO peripheral pin interface.
@@ -29,40 +31,19 @@ GPIOpin_t GPIOpin_init(GPIO_TypeDef *port, GPIO_Pin pin, GPIO_Config *config) {
   if (port == NULL)
     return (GPIOpin_t){.port = NULL};
 
+  // Initialise config with default values if passed NULL.
   if (config == NULL) {
     config = &(GPIO_Config){
-        GPIO_MODE_OUTPUT,
-        GPIO_TYPE_PUSHPULL,
-        GPIO_SPEED_HIGH,
-        GPIO_PUPD_NONE,
-        GPIO_AF0,
+        GPIO_MODE_OUTPUT,   // I/O direction output
+        GPIO_TYPE_PUSHPULL, // Push-pull output type
+        GPIO_SPEED_HIGH,    // High speed output
+        GPIO_PUPD_NONE,     // No pull-up, pull-down
+        GPIO_AF0,           // Alternate function 0
     };
   }
 
-  // Get index of supplied port by subtracting GPIOA address and dividing by size
-  int portIndex  = ((uint32_t)port - GPIOA_BASE) / GPIO_PERIPHERAL_SIZE;
-
-  RCC->AHB1ENR  |= (0b01 << portIndex);                      // Enable port in RCC
-  RCC->AHB1RSTR |= (0b01 << portIndex);                      // Reset port
-  __ASM("NOP");                                              //
-  __ASM("NOP");                                              //
-  RCC->AHB1RSTR &= ~(0b01 << portIndex);                     // Clear reset
-
-  port->MODER   &= ~(0b11 << (2 * pin));                     // Clear MODER bits for pin
-  port->MODER   |= (config->mode << (2 * pin));              // Shift in mode bits from config
-
-  port->OTYPER  &= ~(0b01 << pin);                           // Clear OTYPE bits for pin
-  port->OTYPER  |= (config->type << pin);                    // Shift in type bits from config
-
-  port->OSPEEDR &= ~(0b11 << (2 * pin));                     // Clear OSPEEDR bits for pin
-  port->OSPEEDR |= (config->speed << (2 * pin));             // Shift in speed bits from config
-
-  port->PUPDR   &= ~(0b11 << (2 * pin));                     // Clear PUPDR bits for pin
-  port->PUPDR   |= (config->pupd << (2 * pin));              // Shift in pupd bits from config
-
-  uint32_t afr   = (pin <= 7) ? port->AFR[0] : port->AFR[1]; // Select AFRL (pin<=7) or AFRH (pin>7)
-  afr           &= ~(0b1111 << (4 * pin));                   // Clear AFR bits for pin
-  afr           |= (config->afr << (4 * pin));               // Shift in afr bits from config
+  // Initialise GPIO registers and enable peripheral
+  _GPIOpin_init(port, pin, *config);
 
   // Create GPIO struct from parameters and initialise methods
   GPIOpin_t GPIO;
@@ -78,9 +59,60 @@ GPIOpin_t GPIOpin_init(GPIO_TypeDef *port, GPIO_Pin pin, GPIO_Config *config) {
   return GPIO;
 }
 
+// ALLOW FORMATTING
+#ifndef DOXYGEN_PRIVATE
+
 /* =============================================================================== */
 /**
- * @brief Initialiser for a
+ * @brief Private initialiser for GPIO registers.
+ * Enables and resets the GPIO port in RCC and sets configuration registers.
+ *
+ * @param port   Pointer to the GPIO_TypeDef struct representing the pin's port.
+ * @param pin    Enum quantified value of the pin's position in its port.
+ * @param config Pointer to GPIO_Config struct for initial configuration.
+ *               This may be passed as \c NULL to initialise a default
+ *               configuration. @see GPIO_Config
+ *
+ * @return \c NULL.
+ **
+ * =============================================================================== */
+static void _GPIOpin_init(GPIO_TypeDef *port, GPIO_Pin pin, GPIO_Config config) {
+
+  // Get index of supplied port by subtracting GPIOA address and dividing by size
+  int portIndex = ((uint32_t)port - GPIOA_BASE) / GPIO_PERIPHERAL_SIZE;
+
+  // Enable GPIO peripheral and configure port registers for selected pin
+  RCC->AHB1ENR  |= (0b01 << portIndex);                      // Enable port in RCC
+  RCC->AHB1RSTR |= (0b01 << portIndex);                      // Reset port
+  __ASM("NOP");                                              //
+  __ASM("NOP");                                              //
+  RCC->AHB1RSTR &= ~(0b01 << portIndex);                     // Clear reset
+
+  port->MODER   &= ~(0b11 << (2 * pin));                     // Clear MODER bits for pin
+  port->MODER   |= (config.mode << (2 * pin));               // Shift in mode bits from config
+
+  port->OTYPER  &= ~(0b01 << pin);                           // Clear OTYPE bits for pin
+  port->OTYPER  |= (config.type << pin);                     // Shift in type bits from config
+
+  port->OSPEEDR &= ~(0b11 << (2 * pin));                     // Clear OSPEEDR bits for pin
+  port->OSPEEDR |= (config.speed << (2 * pin));              // Shift in speed bits from config
+
+  port->PUPDR   &= ~(0b11 << (2 * pin));                     // Clear PUPDR bits for pin
+  port->PUPDR   |= (config.pupd << (2 * pin));               // Shift in pupd bits from config
+
+  uint32_t afr   = (pin <= 7) ? port->AFR[0] : port->AFR[1]; // Select AFRL (pin<=7) or AFRH (pin>7)
+  afr           &= ~(0b1111 << (4 * pin));                   // Clear AFR bits for pin
+  afr           |= (config.afr << (4 * pin));                // Shift in afr bits from config
+}
+
+#endif
+
+/* =============================================================================== */
+/**
+ * @brief Set the selected GPIO pin
+ * Uses a logic OR to set the pin's current value in the data register.
+ *
+ * @param gpio Pointer to GPIOpin_t struct.
  *
  * @return @c NULL.
  **
@@ -91,7 +123,10 @@ void GPIOpin_set(GPIOpin_t *gpio) {
 
 /* =============================================================================== */
 /**
- * @brief Initialiser for a
+ * @brief Clear the selected GPIO pin
+ * Uses a logic AND to clear the pin's current value in the data register.
+ *
+ * @param gpio Pointer to GPIOpin_t struct.
  *
  * @return @c NULL.
  **
@@ -102,7 +137,10 @@ void GPIOpin_reset(GPIOpin_t *gpio) {
 
 /* =============================================================================== */
 /**
- * @brief Initialiser for a
+ * @brief Toggle the selected GPIO pin
+ * Uses a logic XOR to invert the pin's current value in the data register.
+ *
+ * @param gpio Pointer to GPIOpin_t struct.
  *
  * @return @c NULL.
  **
@@ -113,11 +151,31 @@ void GPIOpin_toggle(GPIOpin_t *gpio) {
 
 /* =============================================================================== */
 /**
- * @brief Initialiser for a
+ * @brief Update GPIO pin configuration
+ * Uses the provided configuration to update the GPIO registers and resets the
+ * associated port in the RCC.
+ * As with initialisation, passing \c NULL will set the default config.
+ *
+ * @param gpio Pointer to GPIOpin_t struct.
  *
  * @return @c NULL.
  **
  * =============================================================================== */
 void GPIOpin_updateConfig(GPIOpin_t *gpio, GPIO_Config *config) {
-  // TODO: implement
+  // Initialise config with default values if passed NULL.
+  if (config == NULL) {
+    config = &(GPIO_Config){
+        GPIO_MODE_OUTPUT,   // I/O direction output
+        GPIO_TYPE_PUSHPULL, // Push-pull output type
+        GPIO_SPEED_HIGH,    // High speed output
+        GPIO_PUPD_NONE,     // No pull-up, pull-down
+        GPIO_AF0,           // Alternate function 0
+    };
+  }
+
+  // Update peripheral with new config
+  gpio->config = *config;
+
+  // Initialise GPIO registers and enable peripheral
+  _GPIOpin_init(gpio->port, gpio->pin, *config);
 }
