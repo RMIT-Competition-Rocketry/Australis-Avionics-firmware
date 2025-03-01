a8bd66c82b682eea4e4a595cd65484a941955f29
Matt Ricci <s3785111@student.rmit.edu.au>
2025-03-01 17:23:58 +1100

    Large restructure of project sources to support greater modularity and extendability.
    Move all Australis "core" sources and includes to Code/core.
    Move all Australis "extra" sources and includes to Code/extra.
    Move all Australis system required files (startup assembly, linker script, etc) to Code/system.
    Remove Core directory.
    Remove unused legacy code.
    Add src/ and inc/ directories to child directories in Target/ for cleaner organisation of target specific code.
    Add directory Test/ with initial child Test/dummy/ for future shell test scripts.
    Relocate dummy data files to appropriate subdirectories in Test/dummy/.
    Drop support for Keil uVision project and MDK-ARM system.
    Move child directories in Middlewares/Third_Party up a directory and remove redundant Third_Party directory.
    Update build system to support changes to project structure and allow greater freedom in modifying and extending the capabilities of the system with target code.

---
+++ Australis-Avionics/Target/AV2/src/tasks.c
@@ -0,0 +1,28 @@
+/***********************************************************************************
+ * @file        tasks.c                                                            *
+ * @author      Matt Ricci                                                         *
+ * @brief                                                                          *
+ ***********************************************************************************/
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "tasklist.h"
+#include "loracomm.h"
+#include "heartbeat.h"
+
+#include "tasks.h"
+
+/* ============================================================================================== */
+/**
+ * @brief Initialise and store FreeRTOS task handles.
+ *
+ * @return .
+ **
+ * ============================================================================================== */
+
+void initTasks() {
+  xTaskCreate(vLoRaSample, "LoRaSample", 256, NULL, configMAX_PRIORITIES - 6, TaskList_new());
+  xTaskCreate(vLoRaTransmit, "LoRaTx", 256, NULL, configMAX_PRIORITIES - 5, TaskList_new());
+  xTaskCreate(vHeartbeatBlink, "HeartbeatBlink", 128, NULL, tskIDLE_PRIORITY + 1, TaskList_new());
+}
